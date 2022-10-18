2abe82b382198f38c0db034a47db3b983eb6c583
techhorizon10 <spoiler24x7@gmail.com>
2022-10-18 19:59:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const select = document.getElementById("select");
+select.addEventListener("click", getSelection);
+
+const input = document.getElementById("input");
+input.addEventListener("input", getFacts);
+
+function getSelection() {
+    const info = document.getElementById("info");
+    info.innerHTML = `<p>Enter a ${select.value} to get a random fact</p>`;
+
+    input.value = "";
+    document.getElementById("fact").innerHTML = "";
+}
+
+function getFacts(e) {
+    if (select.value == "Number") {
+        var selectValue = "trivia";
+    } else {
+        selectValue = select.value.toLowerCase();
+    }
+    fetch(`http://numbersapi.com/${e.target.value}/${selectValue}`)
+        .then((response) => response.text())
+        .then((data) => {
+            const fact = document.getElementById("fact");
+            fact.innerHTML = "";
+            fact.innerHTML += `
+        <h2>${select.value} Fact</h2>
+        <p>${data}</p>
+        `;
+        });
+}
